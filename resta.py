758bedac44111918d8f246eee2c1dd0a634a67f4
edwincervantes2903-star <edwincervantes0104@gmail.com>
2025-12-12 12:32:06 -0600

    resta.py

---
+++ resta.py
@@ -0,0 +1,7 @@
+a = float(input("Ingresa el primer número: "))
+b = float(input("Ingresa el segundo número: "))
+c = float(input("Ingresa el tercer número: "))
+
+resultado = a - b - c
+
+print("El resultado de la resta es:", resultado)
